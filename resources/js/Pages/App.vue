8624f5d3292031ceb835ca3a0018cbbbeb90488b
ArnoczkiAttila <77162976+ArnoczkiAttila@users.noreply.github.com>
2025-01-21 13:59:27 +0100

    asd

---
+++ resources/js/Pages/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+  import { ref } from 'vue';
+  import NewCar from '@/Components/newCar.vue';
+  import newRent from '@/Components/newRent.vue';
+  import showRents from '@/Components/showRents.vue';
+
+  const autok = ref([]);
+  const kolcsonzesek = ref([]);
+
+  const reciveData = (data) => {
+    autok.value.push(data);
+  }
+  const reciveDataFromRents = (data) => {
+    kolcsonzesek.value.push(data);
+  }
+  const megjelenit = ref(false);
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-6">
+        <button class="btn btn-info" @click="megjelenit = !megjelenit">Autó hozzáadása</button>
+        <NewCar v-if="megjelenit"
+          @mentes="reciveData"
+        />
+      </div>
+      <div class="col-6">
+        <newRent
+          @mentes="reciveDataFromRents"
+          :autok="autok"
+        />
+      </div>
+    </div>
+    <div class="row">
+      <showRents
+        :kolcsonzesek="kolcsonzesek"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,8 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
+    return Inertia::render('App', [
+        
     ]);
 });
 
@@ -23,5 +20,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
 
 require __DIR__.'/auth.php';
